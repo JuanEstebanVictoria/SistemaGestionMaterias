f4bd0100a292a21a2ba642e52e65d6e9146bcbb4
Juan Esteban Victoria <juane.victorial@uqvirtual.edu.co>
2025-04-17 14:58:00 -0500

    Update models.py

--- crearUsuario/models.py
+++ crearUsuario/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class RegistroDeUsuario(models.Model):
     nombre = models.CharField(max_length=30, null=False, blank=False, unique=True)
     correo = models.EmailField(blank=False, null=False, unique=True)
-    contraseña = models.SmallIntegerField(unique=True)
+    contraseña = models.SmallIntegerField(max_length=100, blank=False, null= False)
 
     def __str__(self):
         return "{}, {}, {}".format(self.nombre, self.correo, self.contraseña)
